b2269c89fbd0e215b5379aba777b17bcd9fc5c5e
Degtarenko Danil <danil0109200275@gmail.com>
2022-02-14 15:11:50 +0200

    release v2

--- src/Server.ts
+++ src/Server.ts
@@ -1,20 +1,38 @@
 import express from "express";
 import path from "path";
 import 'dotenv/config';
+import { createServer } from "http";
+import { Server } from "socket.io";
+
 
 const app = express();
 const port = process.env.Port;
 
-app.get("/", ( req, res ) => {
+const httpServer = createServer(app);
+const io = new Server(httpServer, { /* options */ });
 
+app.set( "views", path.join( __dirname, "views" ) );
+app.set( "view engine", "ejs" );
+
+app.get("/", ( req, res ) => {
       console.log("server alive");
+      res.render("index");
 } );
+
+io.on('connection', (socket) => {
+   console.log('A user connected');
+
+   // Whenever someone disconnects this piece of code executed
+   socket.on('disconnect',()=> {
+      console.log('A user disconnected');
+   });
+});
 
 app.get( "/echo", ( req, res ) => {
     console.log("server alive echo ");
 } );
 
-app.listen( port, () => {
+httpServer.listen( port, () => {
 
     console.log( `server started at http://localhost:${ port }` );
 } );
